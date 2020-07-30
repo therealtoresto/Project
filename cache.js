130dbb2fb9f06f9afadd119914d232632646b94e
TORESTO <therealtoresto@gmail.com>
2020-07-30 19:52:34 +0300

    fixed error

--- cache.js
+++ cache.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const lib1 = require('./main.js');
-const libPath = require('./main.js');
+const libPath = require.resolve('./main.js');
 console.dir({ libPath });
 delete require.cache[libPath];
 const lib2 = require('./main.js');
